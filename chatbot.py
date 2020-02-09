7fb928605eec3c3827dd494769ef565fe2d11e1b
Alex <alexandra.gil@hotmail.ca>
2020-02-09 12:28:36 -0800

    Removed cleverbot free code and implementation with API

--- chatbot.py
+++ chatbot.py
@@ -3,61 +3,33 @@ Example of a simple CLI script that creates a persistent
 chat session untill closed.
 '''
 
+from cleverwrap import CleverWrap
 import cleverbotfree.cbfree
 import sys
 from gtts import gTTS
 from playsound import playsound
 
 
-cb1 = cleverbotfree.cbfree.Cleverbot()
-cb2 = cleverbotfree.cbfree.Cleverbot()
+englishBot = CleverWrap("API KEY")
+irishBot = CleverWrap("API KEY")
 
 init = True
 
 def main(init_text):
-    try:
-        print("Try connect to browser")
-        cb1.browser.get(cb1.url)
-        cb2.browser.get(cb2.url)
-    except:
-        cb1.browser.close()
-        cb2.browser.close()
-        sys.exit()
-
-    global init
-
-    try:
-        print("try to get form")
-        cb1.get_form()
-        cb2.get_form()
-    except:
-        sys.exit()
-
+    #cw.say("Hello Cleverbot")
+    conv0 = englishBot.new_conversation()
+    conv1 = irishBot.new_converstion()
+    print("Hello There!")
+    text = conv0.say("Hello there!")
     while True:
-        if init == True:
-            bot2 = init_text
-            print ("starting!")
-        init = False
-
-        cb1.send_input(bot2)
-        bot1 = cb1.get_response()
-        tts = gTTS(bot1, lang='en-IE')
-        tts.save("bot.mp3")
-        playsound('bot.mp3')
-        print('Cleverbot1: ', bot1)
-
-        cb2.send_input(bot1)
-        bot2 = cb2.get_response()
-        tts = gTTS(bot2, lang='en-GB')
-        tts.save("bot.mp3")
-        playsound('bot.mp3')
-        print('Cleverbot2: ', bot2)
-
-    cb1.browser.close()
-    cb2.browser.close()
-
-    
+        print(text)
+        text = conv0.say(text)
+        print(text)
+        text = conv1.say(text)
+        
 if __name__ == '__main__':
+
+    conv
     print("input text")
     init_text = input()
     main(init_text)

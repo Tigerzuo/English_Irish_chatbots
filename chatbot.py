8b05c43cc7e791b9f57ec7f90973cdc1ea5f670e
tigerzuo <tigerzuo@gmail.com>
2020-02-17 22:47:29 -0800

    add sound

--- chatbot.py
+++ chatbot.py
@@ -7,23 +7,32 @@ from cleverwrap import CleverWrap
 import sys
 from gtts import gTTS
 from playsound import playsound
+import time
 
 
 englishBot = CleverWrap('')
 irishBot = CleverWrap('')
 
-init = True
-
-def main():
-    conv0 = englishBot.new_conversation()
-    conv1 = irishBot.new_conversation()
-    text = conv0.say("Hello there!")
+def main(text):
     while True:
         print(text)
         text = conv0.say(text)
         print(text)
-        text = conv1.say(text)
-        
-if __name__ == '__main__':
+        tts = gTTS(text, lang='en-IE')
+        tts.save("bot.mp3")
+        playsound('bot.mp3')
 
-    main()
+        time.sleep(0.5)
+
+        text = conv1.say(text)
+        tts = gTTS(text, lang='en-GB')
+        tts.save("bot.mp3")
+        playsound('bot.mp3')
+
+if __name__ == '__main__':
+    conv0 = englishBot.new_conversation()
+    conv1 = irishBot.new_conversation()
+    text = input("Input conversation starter: ")
+    print(text)
+    text = conv1.say(text)
+    main(text)
